74900ec147eb7e5241609d1a6b5793b78516bc73
Sebuuma Daniel <sedzsoft@gmail.com>
2017-12-04 12:30:16 +0300

    Removes google-fluentd installation from the packer/setup.sh as it has been moved to vof_base_image/setup.sh

--- packer/setup.sh
+++ packer/setup.sh
@@ -9,26 +9,18 @@ create_vof_user() {
   fi
 }
 
-start_supervisor_service() {
-  sudo service supervisor start
-}
-
 setup_vof_code() {
-  sudo chown -R vof:vof /home/vof
-  
+  sudo chown -R vof:vof /home/vof 
   cd /home/vof/app && bundle install
 }
 
-install_logging_agent(){
-  # This installs the logging agent into the VM
-  curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
-  sudo dpkg --configure -a && sudo bash install-logging-agent.sh
+start_supervisor_service() {
+  sudo service supervisor start
 }
 
 main() {
   create_vof_user
 
-  install_logging_agent
   setup_vof_code
   start_supervisor_service
 }
